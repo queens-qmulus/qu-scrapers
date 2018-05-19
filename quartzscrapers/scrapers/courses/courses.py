d2ee6292b6e07b604688e18c06ab6e6fdbfccbd9
alexadusei <alex.adusei@gmail.com>
2018-05-19 02:18:32 +0000

    Implement Courses scraper (2/3) - incomplete

--- quartzscrapers/scrapers/courses/courses.py
+++ quartzscrapers/scrapers/courses/courses.py
@@ -32,7 +32,14 @@ class Courses:
 
     IC_ACTIONS = {
         'adv_search': 'DERIVED_CLSRCH_SSR_EXPAND_COLLAPS$149$$1',
+        'term': 'CLASS_SRCH_WRK2_STRM$35$',
+        'class_search': 'CLASS_SRCH_WRK2_SSR_PB_CLASS_SRCH',
         }
+
+    AJAX_PARAMS = {
+        'ICAJAX': '1',
+        'ICNAVTYPEDROPDOWN': '0'
+    }
 
     @staticmethod
     def scrape():
@@ -45,46 +52,76 @@ class Courses:
             'Action': 'U',
             'ExactKeys': 'Y',
             'TargetFrameName': 'None',
-            'adv_search': Courses.IC_ACTIONS['adv_search'],
             }
 
-        # Imitate an actual login and grab generated cookies, which allow us
-        # to bypass SOLUS request redirects.
-        session_cookies = Courses.login()
+        # Imitate an actual login and grab generated cookies, which allows the
+        # bypass of SOLUS request redirects.
+        cookies = Courses.login()
 
         # Note: May not be necessary
         # hidden_params = Courses._get_hidden_params(soup)
         # hidden_params.update(Courses.IC_ACTIONS['adv_search'])
 
-        # TODO: Build in post requests into base scraper request function
+        params.update(Courses._create_ic_action('adv_search'))
 
+        # TODO: Build in post requests into base scraper request function
         # Request 'search' page for list of courses.
         soup = Scraper.http_request(
             Courses.host,
             params=params,
-            cookies=session_cookies,
+            cookies=cookies,
             )
 
         initially_selected_term = Courses._get_selected_term(soup)
         advanced_search_params = Courses._get_advanced_search_params()
-
         params.update(advanced_search_params)
 
-        departments = Courses._get_departments(soup)
+        years_and_terms = _get_years_and_terms(soup)
 
-        import pdb; pdb.set_trace()
+        # TODO: Check/verify from here
+        for year, terms in years_and_terms.items():
+            for term_name, term_code in terms.items():
+                soup = Courses._update_term(term_code, params, cookies)
 
-        print('temporarily done for now')
+                # Update search params to get course list.
+                params = Courses._remove_ajax_params(params)
+                params.update(Courses._create_ic_action('class_search'))
 
-        return
+                departments = Courses._get_departments(soup)
+
+                for dept_code, dept_name in departments.items():
+                    # Update search payload with department code
+                    params['SSR_CLSRCH_WRK_SUBJECT_SRCH$0'] = dept_code
+
+                    # Get course listing page for department
+                    soup.= Scraper.http_request(
+                        Courses.host,
+                        params=params,
+                        cookies=cookies
+                        )
+
+                    # too many results
+                    if not Courses._is_valid_search_page(soup):
+                        soup = Courses._handle_special_case_on_search(soup)
+
+                    courses = Courses._get_courses(soup)
+                    course_soups = Courses._get_course_list_as_soup(
+                        courses, soup)
+
+                    for course_soup in course_soups:
+                        course_data Courses.parse_course_data(course_soup)
+
+                        # TODO: Check & persist data
+
+
 
 
     @staticmethod
     def login():
         '''
         Emulate a SOLUS login via a Selenium webdriver. Mainly used for user
-        authentication. Returns session cookies, which are are retrieved and
-        used for the remainder of this scraping session.
+        authentication. Returns session cookies, which are retrieved and used
+        for the remainder of this scraping session.
 
         Returns:
             Object
@@ -123,6 +160,11 @@ class Courses:
         driver.close()
 
         return session_cookies
+
+
+    @staticmethod
+    def _create_ic_action(action):
+        return {'ICAction': Courses.IC_ACTIONS[action]}
 
 
     # Note: May not be necessary
@@ -214,19 +256,75 @@ class Courses:
 
 
     @staticmethod
-    def foo(bar):
+    def _get_years_and_terms(soup):
+        years_terms_values = {}
+
+        term_data_soup = (
+            soup.find('select', id='CLASS_SRCH_WRK2_STRM$35$')
+                .find_all('option')[1:])
+
+        for term_data in term_data_soup:
+            # differentiate between term name and years
+            year, term = term_data.text.split(' ')
+
+            if year not in years_terms_values:
+                years_terms_values[year] = {}
+
+            # Add corresponding year-term code per term
+            years_terms_values[year][term] = term_data['value']
+
+        return years_terms_values
+
+
+    @staticmethod
+    def _update_term(term_code, params, cookies):
+        # E.g: {'CLASS_SRCH_WRK2_STRM$35$': '2195'}, where 2195 is Summer 2019
+        params[Courses.IC_ACTIONS['term']] = term_code
+
+        # E.g: {'ICAction': 'CLASS_SRCH_WRK2_STRM$35$'}
+        params.update(Courses._create_ic_action('term'))
+        params.update(PeoplesoftParser.AJAX_PARAMS)
+
+        return Courses.http_request(
+            Courses.host,
+            params=params,
+            cookies=cookies
+            )
+
+
+    @staticmethod
+    def _remove_ajax_params(params):
+        for key in AJAX_PARAMS:
+            del params[key]
+
+        return params
+
+
+    @staticmethod
+    def _is_valid_search_page(soup, cookies):
         pass
 
 
     @staticmethod
-    def foo(bar):
+    def _is_special_search(soup, cookies):
         pass
 
 
     @staticmethod
-    def foo(bar):
+    def _handle_special_case_on_search(soup, cookies):
         pass
 
+
     @staticmethod
-    def foo(bar):
+    def _get_courses(soup, cookies):
+        pass
+
+
+    @staticmethod
+    def _get_course_list_as_soup(courses, soup, cookies):
+        pass
+
+
+    @staticmethod
+    def parse_course_data(soup):
         pass
